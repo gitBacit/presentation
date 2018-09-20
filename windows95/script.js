65c047a53f98979089d29418e07f33d4f76df208
mathie16@uia.no <Hjemme8870a>
2018-09-20 20:37:25 +0200

    ny branch

--- windows95/script.js
+++ windows95/script.js
@@ -83,6 +83,7 @@
 	document.getElementById("documents").addEventListener("dblclick", openDocument);
 	document.getElementById("do").addEventListener("dblclick", openDocument);
 	document.getElementById("kryss2").addEventListener("click", closeDocument);
+	//document.getElementById("users2").addEventListener("click", openDocument;
 
 	function openDocument(){
 		document.getElementById("documentsinside").style.visibility="visible";	
